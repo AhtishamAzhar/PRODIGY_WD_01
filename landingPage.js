d0747338ddfd73f515780e283a183c2092cba800
Ahtisham Azhar <ahtishamazhar2003@gmail.com>
2024-07-09 12:10:36 +0500

    first commit

---
+++ landingPage.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.style.backgroundColor = '#444';
+    } else {
+      navbar.style.backgroundColor = '#333';
+    }
+  });
+  
